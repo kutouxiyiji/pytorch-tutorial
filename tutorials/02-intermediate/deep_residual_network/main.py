6ce24bab5afa0f12c6e0e3a30543addfe655e947
kutouxiyiji <y.wu1988@gmail.com>
2025-09-15 09:58:42 -0700

    update the code comments w/ reading

--- tutorials/02-intermediate/deep_residual_network/main.py
+++ tutorials/02-intermediate/deep_residual_network/main.py
@@ -21,9 +21,9 @@ learning_rate = 0.001
 
 # Image preprocessing modules
 transform = transforms.Compose([
-    transforms.Pad(4),
+    transforms.Pad(4), # now it's 32 + 8 = 40, 40 * 40. Pad 4 to 4 sides of the image.
     transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(32),
+    transforms.RandomCrop(32), # crop back to original data
     transforms.ToTensor()])
 
 # CIFAR-10 dataset
@@ -56,7 +56,7 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         self.conv1 = conv3x3(in_channels, out_channels, stride)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=True) # mainly for efficiency, don't make copy but modify the input directly
         self.conv2 = conv3x3(out_channels, out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
@@ -69,7 +69,7 @@ class ResidualBlock(nn.Module):
         out = self.conv2(out)
         out = self.bn2(out)
         if self.downsample:
-            residual = self.downsample(x)
+            residual = self.downsample(x) # because the network's max pooling will reduce the size, downsample to match it.
         out += residual
         out = self.relu(out)
         return out
